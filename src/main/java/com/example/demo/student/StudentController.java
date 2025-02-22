093005ba4f9f6e57ecf1a9054cdc703eafb20b34
ahmad123r <ahmadzero___@hotmail.com>
2025-02-22 22:12:43 +0200

    editing dockerfile

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/student")
+@RequestMapping(path = "api/ahmadv1/student")
 public class StudentController {
     private final StudentService studentService;
 
@@ -32,8 +32,8 @@ public class StudentController {
 
     @PutMapping(path = "{studentId}")
     public void updateStudent(@PathVariable("studentId") Long studentId,
-                              @RequestParam(required = false) String name,
-                              @RequestParam(required = false) String email){
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
         studentService.updateStudent(studentId, name, email);
     }
 }
